b47ae961623bb778aa8110427e1557d327f49698
Konstantin Skidan <k.skidan@ksit.com.ua>
2018-09-20 21:55:03 +0300

    added required routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 // pages
+import AppLayout from "./pages/AppLayout";
 import HomePage from './pages/HomePage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 export default (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-    </Switch>
+    <AppLayout titleNavbar="Bla-Bla-Blog">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/posts" component={PostListPage} />
+        <Route exact path="/posts/:id" component={PostPage} />
+        <Redirect to="/" /> {/* redirect 404 status code */}
+      </Switch>
+    </AppLayout>
   </BrowserRouter>
-);
+)
