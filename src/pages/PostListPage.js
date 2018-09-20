2da8822732e4d827d735611360bef39258de0c3b
Konstantin Skidan <k.skidan@ksit.com.ua>
2018-09-20 14:02:37 +0300

    added required pages

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class PostListPage extends React.Component {
+    render() {
+        return (
+            <div>Post list page</div>
+        )
+    }
+}
+
+export default PostListPage
