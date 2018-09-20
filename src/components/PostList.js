45ef7914e1cf741dca346031f23dfeffe8259cc0
Konstantin Skidan <k.skidan@ksit.com.ua>
2018-09-21 00:48:00 +0300

    fixed filter

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,13 +4,43 @@ import PostFilter from "./PostFilter";
 
 class PostList extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      filteredPosts: this.props.posts
+    }
+
+  }
+
   handlerFilter(query) {
-    console.log(query)
+
+    console.log('filter', query)
+
+    let filtered = this.props.posts
+
+    if (query) {
+      
+      filtered = filtered.filter((item) => {
+        let { title, body } = item
+        return (title && body) ? title.indexOf(query) !== -1 || body.indexOf(query) !== -1 : false
+
+      })
+
+    }
+
+    console.log('filtered', filtered)
+
+    this.setState({
+      filteredPosts: filtered
+    })
+
   }
 
   render() {
 
-    var { posts } = this.props
+    var posts = this.state.filteredPosts
+    console.log('render posts', posts)
 
     // list of posts
     const listItems = posts.map((item, idx) => {
