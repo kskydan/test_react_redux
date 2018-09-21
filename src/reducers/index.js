032b92363cbd8dd581a4f1f735d9a1fc1f61019a
Konstantin Skidan <k.skidan@ksit.com.ua>
2018-09-21 04:53:07 +0300

    added user, comments reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+// reducers
 import PostsReducer from "./PostsReducer";
+import UsersReducer from "./UsersReducer";
+import CommentssReducer from "./CommentsReducer";
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  users: UsersReducer,
+  comments: CommentssReducer,
   form: formReducer
 })
 
